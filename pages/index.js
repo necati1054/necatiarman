07ffbd27368fb5ca3d0903b86d29312de5f2f667
necati1054 <59114685+necati1054@users.noreply.github.com>
2019-12-28 16:04:04 +0300

    dde

--- pages/index.js
+++ pages/index.js
@@ -28,6 +28,15 @@ const Home = ({ posts }) => (
     </div>
     {posts.map(post => (
       <div className="blog">
+        <h2 className="blog-title">
+          <Link href={post.slug}>
+            <a className="blog-title-link">{post.title}</a>
+          </Link>
+        </h2>
+        <div className="blog-text">
+          <ReactMarkdown source={post.details} />
+        </div>
+        <div className="blog-date">{post.date}</div>
       </div>
     ))}
 
