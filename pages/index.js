9acdc1bb41d5fa7d6529c04f0a911c29444eed7b
necati1054 <59114685+necati1054@users.noreply.github.com>
2019-12-28 16:06:48 +0300

    gfdh

--- pages/index.js
+++ pages/index.js
@@ -28,15 +28,7 @@ const Home = ({ posts }) => (
     </div>
     {posts.map(post => (
       <div className="blog">
-        <h2 className="blog-title">
-          <Link href={post.slug}>
-            <a className="blog-title-link">{post.title}</a>
-          </Link>
-        </h2>
-        <div className="blog-text">
-          <ReactMarkdown source={post.details} />
-        </div>
-        <div className="blog-date">{post.date}</div>
+        <h2 className="blog-title"></h2>
       </div>
     ))}
 
