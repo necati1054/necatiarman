29094f3b886d35d09505019a09e269850a6e395a
necati1054 <59114685+necati1054@users.noreply.github.com>
2019-12-28 21:58:03 +0300

    ıyou

--- pages/index.js
+++ pages/index.js
@@ -26,16 +26,6 @@ const Home = ({ posts }) => (
         </div>
       </div>
     </div>
-    {posts.map(post => (
-      <div className="blog">
-        <h2 className="blog-title">
-          <Link href={post.slug}>
-            <a className="blog-title-link">{post.title}</a>
-          </Link>
-        </h2>
-      </div>
-    ))}
-
     <style jsx>{`
       .container {
         font-family: 'Indie Flower', cursive;
