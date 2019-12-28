6bb364619248e397d2313b77964f9ff31e79e8ef
necati1054 <59114685+necati1054@users.noreply.github.com>
2019-12-28 15:42:58 +0300

    asdf

--- pages/index.js
+++ pages/index.js
@@ -26,20 +26,6 @@ const Home = ({ posts }) => (
         </div>
       </div>
     </div>
-    {posts.map(post => (
-      <div className="blog">
-        <h2 className="blog-title">
-          <Link href={post.slug}>
-            <a className="blog-title-link">{post.title}</a>
-          </Link>
-        </h2>
-        <div className="blog-text">
-          <ReactMarkdown source={post.details} />
-        </div>
-        <div className="blog-date">{post.date}</div>
-      </div>
-    ))}
-
     <style jsx>{`
       .container {
         width: 100%;
